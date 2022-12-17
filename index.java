eb6bab00756f1441f29c5236a645b8c7d81dc967
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2022-12-17 12:05:40 +0530

    Update index.java

--- index.java
+++ index.java
@@ -3,28 +3,28 @@ import java.util.*;
 public class index{
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        // to find the even Or Odd number 
+                                                     // to find the even Or Odd number 
         // System.out.println("enter the value ");
         // int number = sc.nextInt();
-        // if(number % 2 == 0){
-        //    System.out.println("EVEN");
-        // }else{
-        //     System.out.println("ODD");
-        // }
+        if(number % 2 == 0){
+           System.out.println("EVEN");
+        }else{
+            System.out.println("ODD");
+        }
 
-        // to find the great value between three 
+                                                  // to find the great value between three 
         // int  a = sc.nextInt();
         // int b = sc.nextInt();
         // int c = sc.nextInt();
-        // if(a>b && a>c){
-        //     System.out.println("a is greater ");
-        // }else if(b>a && b>c){
-        //      System.out.println("b is greater");
-        // }else{
-        //     System.out.println("c is greater");
-        // }
+        if(a>b && a>c){
+            System.out.println("a is greater ");
+        }else if(b>a && b>c){
+             System.out.println("b is greater");
+        }else{
+            System.out.println("c is greater");
+        }
 
-        char  a = sc.next().charAt(0); //this is how to take char as input 
+        char  a = sc.next().charAt(0);                           //this is how to take char as input 
         if(Character.isUpperCase(a)){
             System.out.println("1");
         } else if(Character.isLowerCase(a)){
